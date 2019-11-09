42124f65183956acbc80b5ad0b0bf05cc31c4a14
alexis faivret <alexis.faivret@gmail.com>
2019-11-09 22:22:18 +0100

    info: add some comment line to explain the code

--- src/sendMail.py
+++ src/sendMail.py
@@ -5,6 +5,7 @@ import log
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Load the mail server configuration ( user mailconfig.json template )
 with open(BASE_DIR + '/config/mailconfig.json') as config_json:
     config = json.load(config_json)
 
@@ -13,18 +14,21 @@ with open(BASE_DIR + '/config/mailconfig.json') as config_json:
     PASSWD = config['mail']['PASSWD']
     PORT = config['mail']['PORT']
 
+# Load contact list ( use contact.json template )
 with open(BASE_DIR + '/config/contact.json') as contact_json:
     contacts = json.load(contact_json)
     for contact in contacts:
         CONTACT_NAME = contacts[contact]['NAME']
         CONTACT_ADRESS = contacts[contact]['ADRESS']
 
+        # Add server info and start SSL/TLS
         s = smtplib.SMTP(host=HOST, port=PORT)
         s.starttls()
 
         try:
             log.writeLogFile(
                 'mail', 'info', '### Try connecting to server ###')
+            # Try to log to the mail server
             s.login(MY_ADRESS, PASSWD)
             log.writeLogFile(
                 'mail', 'info', 'Identification success')
@@ -38,8 +42,9 @@ with open(BASE_DIR + '/config/contact.json') as contact_json:
             break
 
         message = "The message to put in the mail"
+        # Send the mail or not obviously
         if not s.sendmail(MY_ADRESS, CONTACT_ADRESS, message):
-            log.writeLogFile('mail', 'info', 'mail test envoyer')
+            log.writeLogFile('mail', 'info', 'The mail has been sent')
         else:
-            log.writeLogFile('mail', 'info', '')
+            log.writeLogFile('mail', 'critical', 'Failed to send mail')
         s.quit()
