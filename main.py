322acf31b493e3572b6915b174e06ee4575e1d05
alexis faivret <alexis.faivret@gmail.com>
2019-11-11 13:03:41 +0100

    feat: added threading support feat: added treatment in case of non-recognition of a person 5 times in a row info: added mail and log script in main.py

--- main.py
+++ main.py
@@ -1,117 +1,119 @@
-import numpy as np
-import cv2
+
+import os
 import pickle
+import sys
+import time
+from threading import Thread
+
+import cv2
 import imutils
+import numpy as np
 
-# CONF CASCADE
-FACE_CASCADE = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
-
-EYES_CASCADE = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_eye.xml')
-
-FULLBODY_CASCADE = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_fullbody.xml')
-
-# CONF RECOGNITION
-RECOGNIZER = cv2.face.LBPHFaceRecognizer_create()
-RECOGNIZER.read("src/trainner.yml")
-
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-
-labels = {"person_name": 1}
-
-with open("src/lables.pickle", 'rb') as f:
-    og_labels = pickle.load(f)
-    labels = {v: k for k, v in og_labels.items()}
-
-# CAMERA PARAMS
-camera = cv2.VideoCapture(0)
-
-# Camera resolution
-camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-# camera FPS
-camera.set(cv2.CAP_PROP_FPS, 60)
+from src.sendMail import *
 
 
-# FONCTIONS
+class Detection(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.state = True
 
-def FaceDetection():
-    faces = FACE_CASCADE.detectMultiScale(
-        gray, scaleFactor=1.3, minNeighbors=1)
+        ''' 
+        CASCADE CONFIGURATION
+        You can add here any cascade configuration you want to use. I only use FACE and EYES for now
+        '''
+        self.FACE_CASCADE = cv2.CascadeClassifier(
+            cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
 
-    for(x, y, z, h) in faces:
-        color_face = (0, 200, 0)  # BGR 0-255
-        stroke = 2
-        end_coord_x = x + z
-        end_coord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y),
-                      color_face, stroke)
-    FacialRecognition(faces)
+        self.EYES_CASCADE = cv2.CascadeClassifier(
+            cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+        # Device used for video input
+        self.camera = cv2.VideoCapture(0)
+
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.flag = 0
+
+    def run(self):
+
+        # CONF RECOGNITION
+        RECOGNIZER = cv2.face.LBPHFaceRecognizer_create()
+        RECOGNIZER.read(self.BASE_DIR + "/src/trainner.yml")
+
+        hog = cv2.HOGDescriptor()
+        hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+        labels = {"person_name": 1}
+
+        with open(self.BASE_DIR + "/src/lables.pickle", 'rb') as f:
+            og_labels = pickle.load(f)
+            labels = {v: k for k, v in og_labels.items()}
+
+        # Camera resolution
+        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        # camera FPS
+        self.camera.set(cv2.CAP_PROP_FPS, 30)
+
+        # FONCTIONS
+
+        def FaceDetection(frame, gray):
+            faces = self.FACE_CASCADE.detectMultiScale(
+                gray, scaleFactor=1.3, minNeighbors=3)
+
+            for(x, y, z, h) in faces:
+                color_face = (0, 200, 0)  # BGR 0-255
+                stroke = 2
+                end_coord_x = x + z
+                end_coord_y = y + h
+                cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y),
+                              color_face, stroke)
+                FacialRecognition(faces, gray, frame)
+
+        def FacialRecognition(faces, gray, frame):
+            for(x, y, z, h) in faces:
+                roi_gray = gray[y:y+h, x:x+z]
+
+                # reconaissance
+                id_, conf = RECOGNIZER.predict(roi_gray)
+                if conf < 95:
+                    print(labels[id_])
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    name = labels[id_]
+                    color = (255, 0, 0)
+                    stroke = 1
+                    cv2.putText(frame, name, (x, y), font, 1,
+                                color, stroke, cv2.LINE_AA)
+                else:
+                    # Do something when a person has been detected but not reconized
+                    print(' Unkown person detected')
+                    self.flag += 1
+                    if self.flag == 5:
+                        print('Flag at 5')
+                        self.flag = 0
+
+        while(self.state == True):
+                # capture frame par frame
+            ret, frame = self.camera.read()
+
+            # converstion to gray shape
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            FaceDetection(frame, gray)
+
+            # Display frame
+            cv2.imshow('Reconaissance faciale', frame)
+            cv2.namedWindow('Reconaissance faciale', cv2.WINDOW_OPENGL)
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
+
+            # When done, we stop capturing and release camera
+        self.camera.release()
+        cv2.destroyAllWindows()
+
+    def stop(self):
+        self.state = False
 
 
-def FacialRecognition(faces):
-    for(x, y, z, h) in faces:
-        roi_gray = gray[y:y+h, x:x+z]
-
-        # reconaissance
-        id_, conf = RECOGNIZER.predict(roi_gray)
-        if conf < 98:
-            print(labels[id_])
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            name = labels[id_]
-            color = (255, 0, 0)
-            stroke = 1
-            cv2.putText(frame, name, (x, y), font, 1,
-                        color, stroke, cv2.LINE_AA)
-        else:
-            print(' Unkown person detected')
-            # here need to implement the mail to tell that a unkown person has been detected but need to send it once.
-
-
-def EyesDetection():
-    eye = EYES_CASCADE.detectMultiScale(
-        gray, scaleFactor=3.4, minNeighbors=2)
-
-    for(x, y, z, h) in eye:
-        color_face = (255, 0, 0)  # BGR 0-255
-        stroke = 2
-        end_coord_x = x + z
-        end_coord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y),
-                      color_face, stroke)
-
-
-def FullBodyDetection():
-    fullBody = FULLBODY_CASCADE.detectMultiScale(
-        gray, scaleFactor=1.3, minNeighbors=1)
-
-    for(x, y, z, h) in fullBody:
-        color_body = (0, 0, 255)
-        stroke = 2
-        end_coord_x = x + z
-        end_coord_y = y + h
-        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y),
-                      color_body, stroke)
-
-
-while(True):
-    # capture frame par frame
-    ret, frame = camera.read()
-
-    # converstion to gray shape
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    FaceDetection()
-
-    # Display frame
-    cv2.imshow('Reconaissance faciale', frame)
-    cv2.namedWindow('Reconaissance faciale', cv2.WINDOW_OPENGL)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
-# When done, we stop capturing and release camera
-camera.release()
-cv2.destroyAllWindows()
+# Starting detection thread
+detection = Detection()
+detection.start()
--- src/sendMail.py
+++ src/sendMail.py
@@ -1,46 +1,49 @@
 import smtplib
 import json
 import os
-import log
+from src.log import *
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Load the mail server configuration ( user mailconfig.json template )
-with open(BASE_DIR + '/config/mailconfig.json') as config_json:
-    config = json.load(config_json)
 
-    HOST = config['mail']['HOST']
-    MY_ADRESS = config['mail']['USER']
-    PASSWD = config['mail']['PASSWD']
-    PORT = config['mail']['PORT']
+def sendMail():
+
+    # Load the mail server configuration ( user mailconfig.json template )
+    with open(BASE_DIR + '/config/mailconfig.json') as config_json:
+        config = json.load(config_json)
+
+        HOST = config['mail']['HOST']
+        MY_ADRESS = config['mail']['USER']
+        PASSWD = config['mail']['PASSWD']
+        PORT = config['mail']['PORT']
 
 # Load contact list ( use contact.json template )
-with open(BASE_DIR + '/config/contact.json') as contact_json:
-    contacts = json.load(contact_json)
-    for contact in contacts:
-        CONTACT_NAME = contacts[contact]['NAME']
-        CONTACT_ADRESS = contacts[contact]['ADRESS']
+    with open(BASE_DIR + '/config/contact.json') as contact_json:
+        contacts = json.load(contact_json)
+        for contact in contacts:
+            CONTACT_NAME = contacts[contact]['NAME']
+            CONTACT_ADRESS = contacts[contact]['ADRESS']
 
         # Add server info and start SSL/TLS
-        s = smtplib.SMTP(host=HOST, port=PORT)
-        s.starttls()
+            s = smtplib.SMTP(host=HOST, port=PORT)
+            s.starttls()
 
-        try:
-            # Try to log to the mail server
-            s.login(MY_ADRESS, PASSWD)
-        except smtplib.SMTPAuthenticationError:
-            log.writeLogFile(
-                'mail', 'critical', 'FAILED: Server retuned bad username or password')
-            break
-        except smtplib.SMTPException:
-            log.writeLogFile(
-                'mail', 'critical', 'FAILED: SERVER ABOUT TO DESTROY HUMANITY')
-            break
+            try:
+                # Try to log to the mail server
+                s.login(MY_ADRESS, PASSWD)
+            except smtplib.SMTPAuthenticationError:
+                log.writeLogFile(
+                    'mail', 'critical', 'FAILED: Server retuned bad username or password')
+                break
+            except smtplib.SMTPException:
+                log.writeLogFile(
+                    'mail', 'critical', 'FAILED: SERVER ABOUT TO DESTROY HUMANITY')
+                break
 
-        message = "The message to put in the mail"
-        # Send the mail or not obviously
-        if not s.sendmail(MY_ADRESS, CONTACT_ADRESS, message):
-            log.writeLogFile('mail', 'info', 'The mail has been sent')
-        else:
-            print('An error occured. Check log !')
-        s.quit()
+            message = "The message to put in the mail"
+            # Send the mail or not obviously
+            if not s.sendmail(MY_ADRESS, CONTACT_ADRESS, message):
+                log.writeLogFile('mail', 'info', 'The mail has been sent')
+            else:
+                print('An error occured. Check log !')
+            s.quit()
